0438a5ce17c9d0d970bf7767f012a247f751b149
Serge Desmedt <sergedesmedt@yahoo.com>
2018-03-12 21:45:47 +0100

    Translation

---
+++ SceneExperiments/Assets/Scripts/Translate.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translate : MonoBehaviour {
+
+    bool _doAnimation = false;
+    bool _setExtremes = false;
+
+    float _startValue = 0.0f;
+    float _endValue = 3.0f;
+    float _distance = 3.0f;
+
+    static float t = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+        StartAnimation();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!_doAnimation)
+            return; 
+
+        if(_setExtremes) {
+            _startValue = this.transform.position.x;
+            _endValue = _startValue + _distance;
+
+            _setExtremes = false;
+        }
+
+        this.transform.position = new Vector3(Mathf.Lerp(_startValue, _endValue, t), 0, 0);
+
+        t += 0.5f * Time.deltaTime;
+        if (t > 1.0f) {
+            StopAnimation();
+        }
+    }
+
+    public void StartAnimation() {
+        _doAnimation = true;
+        _setExtremes = true;
+    }
+
+    public void StopAnimation() {
+        _doAnimation = false;
+    }
+}
