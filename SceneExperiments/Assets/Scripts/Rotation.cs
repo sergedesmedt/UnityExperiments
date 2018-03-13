f1753d93726553e3f7aaa20427673991b68c2dbd
Serge Desmedt <sergedesmedt@yahoo.com>
2018-03-13 19:43:18 +0100

    Rotation through Euler Angles

--- SceneExperiments/Assets/Scripts/Rotation.cs
+++ SceneExperiments/Assets/Scripts/Rotation.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Translate : MonoBehaviour {
+public class Rotation : MonoBehaviour {
 
     bool _doAnimation = false;
     bool _setExtremes = false;
 
     float _startValue = 0.0f;
-    float _endValue = 3.0f;
-    float _distance = 3.0f;
+    float _endValue = 90.0f;
+    float _distance = 90.0f;
 
     static float t = 0.0f;
 
@@ -17,21 +17,23 @@ public class Translate : MonoBehaviour {
     void Start () {
         StartAnimation();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 
         if (!_doAnimation)
-            return; 
+            return;
 
-        if(_setExtremes) {
-            _startValue = this.transform.position.x;
+        if (_setExtremes) {
+            //_startValue = this.transform.eulerAngles.y;
+            _startValue = this.transform.localEulerAngles.y;
             _endValue = _startValue + _distance;
 
             _setExtremes = false;
         }
 
-        this.transform.position = new Vector3(Mathf.Lerp(_startValue, _endValue, t), 0, 0);
+        //this.transform.eulerAngles = new Vector3(this.transform.eulerAngles.x, Mathf.Lerp(_startValue, _endValue, t), this.transform.eulerAngles.z);
+        this.transform.localEulerAngles = new Vector3(this.transform.localEulerAngles.x, Mathf.Lerp(_startValue, _endValue, t), this.transform.localEulerAngles.z);
 
         t += 0.5f * Time.deltaTime;
         if (t > 1.0f) {
